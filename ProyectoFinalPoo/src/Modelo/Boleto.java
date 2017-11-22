3f60e1fe41ac2261afbe4343c04e07ecbab08101
HumbertoPech <humbertopech30@gmail.com>
2017-11-22 14:37:40 -0600

    Carpeta del proyecto
    carpeta POO

---
+++ ProyectoFinalPoo/src/Modelo/Boleto.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+public class Boleto {
+
+    private static int precio;
+    private Asiento asiento;
+    private Funcion funcion;
+    
+    public Boleto(int precio,Asiento asiento,Funcion f){
+        this.precio=precio;
+        this.asiento=asiento;
+        this.funcion=f;
+    }
+    public static int getPrecio() {
+        return precio;
+    }
+
+    public static void setPrecio(int precio) {
+        Boleto.precio = precio;
+    }
+}
---
+++ ProyectoFinalPoo/src/Modelo/Sala.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Humberto
+ */
+public class Sala {
+    int numeroAsientos;
+    Asiento asientos[][];
+    
+    public Sala(){
+     numeroAsientos=25;
+     asientos= new Asiento [5][5];
+ }
+
+    public int getNumeroAsientos() {
+        return numeroAsientos;
+    }
+
+    public Asiento[][] getAsientos() {
+        return asientos;
+    }
+    
+
+}

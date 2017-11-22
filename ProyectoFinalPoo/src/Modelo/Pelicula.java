fb57888fa234603de9e0ee0f3ab10bfda9fc26a8
JustKath <k.broca@hotmail.com>
2017-11-22 14:59:20 -0600

    zs
    pelicula

---
+++ ProyectoFinalPoo/src/Modelo/Pelicula.java
@@ -0,0 +1,19 @@
+
+package Modelo;
+
+
+public class Pelicula {
+    
+    private String nombrePelicula;
+    
+    
+    public Pelicula(String anombrePelicula){
+        
+        nombrePelicula = anombrePelicula;
+        
+    }
+    
+    public String getNombrePelicula(){
+        return nombrePelicula;
+    }
+}
